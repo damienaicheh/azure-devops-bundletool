83f57ca7fd6a53679bf572c24493d43cc0f9e0e0
Damien AICHEH <damien.aicheh@gmail.com>
2021-11-15 09:54:01 +0100

    feat: Update to Node 10.

--- tasks/InstallBundletool/run.sh
+++ tasks/InstallBundletool/run.sh
@@ -1,4 +1,4 @@
-export INPUT_ReleaseVersionTag="0.13.4" &&
+export INPUT_ReleaseVersionTag="1.8.2" &&
 export INPUT_Username="username" &&
 export INPUT_PersonalAccessToken="accesstoken" &&
 tsc && node install-bundletool.js
